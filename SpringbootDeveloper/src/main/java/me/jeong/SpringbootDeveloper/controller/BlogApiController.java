e5d834d0ab75667a99b67dd4e4bfa0928f144a61
Jeong <50260349+184057@users.noreply.github.com>
2025-03-17 22:43:16 +0900

    Add files via upload

--- SpringbootDeveloper/src/main/java/me/jeong/SpringbootDeveloper/controller/BlogApiController.java
+++ SpringbootDeveloper/src/main/java/me/jeong/SpringbootDeveloper/controller/BlogApiController.java
@@ -3,12 +3,17 @@ package me.jeong.SpringbootDeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.jeong.SpringbootDeveloper.domain.Article;
 import me.jeong.SpringbootDeveloper.dto.AddArticleRequest;
+import me.jeong.SpringbootDeveloper.dto.ArticleResponse;
 import me.jeong.SpringbootDeveloper.service.BlogService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +27,16 @@ public class BlogApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- SpringbootDeveloper/src/main/java/me/jeong/SpringbootDeveloper/dto/ArticleResponse.java
+++ SpringbootDeveloper/src/main/java/me/jeong/SpringbootDeveloper/dto/ArticleResponse.java
@@ -1,4 +1,15 @@
 package me.jeong.SpringbootDeveloper.dto;
 
+import lombok.Getter;
+import me.jeong.SpringbootDeveloper.domain.Article;
+
+@Getter
 public class ArticleResponse {
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
 }
